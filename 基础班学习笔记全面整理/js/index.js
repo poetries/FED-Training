f4ecbf740ed15d701a2d14d96a2c5aedec9d1346
poetries <jingguanliuye@gmail.com>
2016-03-21 07:48:59 +0800

    update

---
+++ 基础班学习笔记全面整理/js/index.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function(){
+			//点击内容ul时
+			$("#menu_left .con").click(function(e){
+				e.stopPropagation();
+			});
+			//点击内容li时
+			$("#menu_left .con li").click(function(){
+				$(this).addClass("curr").siblings().removeClass("curr");
+			});
+			//鼠标滑过内容li时
+			$("#menu_left .con li").mouseover(function(){
+				$(this).addClass("hover").siblings().removeClass("hover");
+			});
+			
+			//点击标签li时
+			$("#menu_left .tab li").click(function(){
+				$(this).find(".con").slideToggle();
+				$(this).siblings().find(".con").slideUp();
+			});
+		});
